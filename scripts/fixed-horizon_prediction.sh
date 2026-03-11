cfe450d733616ef902425b9c737beb3be910023e
LiweiDeng <1481741147@qq.com>
2026-03-11 20:13:00 +0800

    update sh

--- scripts/fixed-horizon_prediction.sh
+++ scripts/fixed-horizon_prediction.sh
@@ -1,18 +1,17 @@
-
 for len in 3 6 12
 do
-for num in 11
+for num in 9
 do
 for rate in 0.0005 0.0001 
 do
 python -u main.py \
   --model_name 'STD2Vformer'\
-  --data_name 'PEMS04' \
+  --data_name 'METR-LA' \
   --points_per_hour 12 \
   --lr $rate \
   --M $num\
   --pred_len $len\
-  --batch_size 32 \
+  --batch_size 16 \
   --resume_dir None\
   --output_dir None\
   --info 'None'\
@@ -21,9 +20,10 @@ done
 done
 done
 
+
 for len in 3 6 12
 do
-for num in 10
+for num in 6
 do
 for rate in 0.0005 0.0001 
 do
@@ -45,18 +45,18 @@ done
 
 for len in 3 6 12
 do
-for num in 2 8 10
+for num in 4 7
 do
 for rate in 0.0005 0.0001 
 do
 python -u main.py \
   --model_name 'STD2Vformer'\
-  --data_name 'METR-LA' \
+  --data_name 'PEMS04' \
   --points_per_hour 12 \
   --lr $rate \
   --M $num\
   --pred_len $len\
-  --batch_size 16 \
+  --batch_size 32 \
   --resume_dir None\
   --output_dir None\
   --info 'None'\
